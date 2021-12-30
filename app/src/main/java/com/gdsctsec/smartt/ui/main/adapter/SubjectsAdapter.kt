ec481a27778e7d27b05da387700ef6582842e3ad
Hriday Keswani <hridaykeswani@gmail.com>
2021-12-30 14:01:52 +0530

    made dynamic color setting of list_item of home fragment

--- app/src/main/java/com/gdsctsec/smartt/ui/main/adapter/SubjectsAdapter.kt
+++ app/src/main/java/com/gdsctsec/smartt/ui/main/adapter/SubjectsAdapter.kt
@@ -1,9 +1,13 @@
 package com.gdsctsec.smartt.ui.main.adapter
 
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.DrawableCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.gdsctsec.smartt.R
 
@@ -18,8 +22,36 @@ class SubjectsAdapter(val subjects: List<String>, val time: List<String>) :
     }
 
     override fun onBindViewHolder(holder: SubjectViewHolder, position: Int) {
+        //val layt:ConstraintLayout = holder.itemView.findViewById(R.id.list_item)
         holder.subject.text = subjects[position]
         holder.time.text = subjects[position]
+        //val border = DrawableCompat.wrap(layt.background)
+
+        when (position % 6) {
+            0 -> {
+                holder.itemView.background.setTint(Color.parseColor("#33FD50C2"))
+            }
+
+            1 -> {
+                holder.itemView.background.setTint(Color.parseColor("#33ECDA35"))
+            }
+
+            2 -> {
+                holder.itemView.background.setTint(Color.parseColor("#3347EFE5"))
+            }
+
+            3 -> {
+                holder.itemView.background.setTint(Color.parseColor("#333D27C6"))
+            }
+
+            4 -> {
+                holder.itemView.background.setTint(Color.parseColor("#332694AC"))
+            }
+
+            5 -> {
+                holder.itemView.background.setTint(Color.parseColor("#33DB2B2B"))
+            }
+        }
     }
 
     override fun getItemCount(): Int {
