c16b7fd978f4474349ba586836cd03b21e1c63e3
Puneet Shetty <puneet22088@gmail.com>
2021-12-30 23:52:57 +0530

    Layout of TT scheduling screen completed

--- app/src/main/java/com/gdsctsec/smartt/TTSchedulingScreen.kt
+++ app/src/main/java/com/gdsctsec/smartt/TTSchedulingScreen.kt
@@ -1,28 +1,52 @@
 package com.gdsctsec.smartt
 
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import android.text.Html
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 
 
 class TTSchedulingScreen : Fragment() {
     // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
     }
 
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_t_t_scheduling_screen, container, false)
+        val view:View= inflater.inflate(R.layout.fragment_t_t_scheduling_screen, container, false)
+
+        val title:TextView=view.findViewById(R.id.remindi_text_view);
+        val word:Spannable=SpannableString("Rem")
+        word.setSpan(ForegroundColorSpan(Color.BLACK),0,word.length,Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        title.setText(word)
+
+        val wordTwo:Spannable=SpannableString("i")
+        wordTwo.setSpan(ForegroundColorSpan(ContextCompat.getColor(requireContext(),R.color.remindi_icon_i)),0,wordTwo.length,Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        title.append(wordTwo)
+
+        val wordThree:Spannable=SpannableString("ndi")
+        wordThree.setSpan(ForegroundColorSpan(Color.BLACK),0,wordThree.length,Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        title.append(wordThree)
+
+        return view;
     }
 
 
