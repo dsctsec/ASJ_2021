7802bcf09c248e715e44ff84db50d23a3694d74b
Krishana Dave <krishnadave671@gmail.com>
2021-12-30 14:14:07 +0530

    Editscreen Activity for review

--- app/src/main/java/com/gdsctsec/smartt/ui/main/MainActivity.kt
+++ app/src/main/java/com/gdsctsec/smartt/ui/main/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.gdsctsec.smartt.ui.main
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.view.get
 import androidx.viewpager2.widget.ViewPager2
+import com.gdsctsec.smartt.EditScreenActivity
 import com.gdsctsec.smartt.R
 import com.gdsctsec.smartt.ui.main.adapter.ViewPagerAdapter
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     lateinit var viewPager: ViewPager2
@@ -15,7 +18,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation_view);
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation_view)
+        val fab_gotoeditscreen : FloatingActionButton =  findViewById(R.id.fab_editTTscreen)
         bottomNavigationView.menu.get(1).isEnabled = false;
         //setting up the viewPager for bottom nav
         viewPager = findViewById(R.id.view_pager)
@@ -36,6 +40,9 @@ class MainActivity : AppCompatActivity() {
                 else -> false
 
             }
+        }
+        fab_gotoeditscreen.setOnClickListener{
+            startActivity(Intent(applicationContext,EditScreenActivity::class.java))
         }
 
         supportActionBar?.hide()
